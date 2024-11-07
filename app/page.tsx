0b88eced05c37b107bc3ad711d1e67d82f2eb69b
Brian Muniz Silveira <155079481+BrianMunizSilveira@users.noreply.github.com>
2024-11-07 19:56:10 -0300

    chore: add git commit message linter

--- app/page.tsx
+++ app/page.tsx
@@ -5,13 +5,13 @@ import { auth } from "@clerk/nextjs/server";
 const Home = async () => {
   const { userId } = await auth();
   if (!userId) {
-    redirect('/login')
+    redirect("/login");
   }
   return (
-    <div className="h-full flex items-center justify-center">
+    <div className="flex h-full items-center justify-center">
       <UserButton showName />
     </div>
-  ) 
-}
+  );
+};
 
 export default Home;
